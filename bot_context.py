7533be741b3c432e7f9c74335ec6896025000fec
Winchestor2001 <sbexruz2001@gmail.com>
2024-03-13 15:46:12 +0500

    update database

---
+++ bot_context.py
@@ -0,0 +1,18 @@
+languages = {
+    "uz": {
+        "start_command_text": "Quyidagilardan birini tanlang",
+
+        "buttons": {
+            "settings_text": "⚙️ Sozlamalar",
+            "menu_text": "🍴 Menu",
+        }
+    },
+    "ru": {
+        "start_command_text": "Выберите одно из следующих",
+
+        "buttons": {
+            "settings_text": "⚙️ Настройки",
+            "menu_text": "🍴 Меню",
+        }
+    },
+}
--- db/database.py
+++ db/database.py
@@ -14,8 +14,27 @@ async def create_tables():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
         username VARCHAR(100) NOT NULL,
-        lang VARCHAR(10) NOT NULL
+        lang VARCHAR(10)
     )''')
+
+
+
+async def add_user(user_id: int, username: str):
+    con, cur = await sql_connector()
+
+    user = cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)).fetchone()
+
+    if not user:
+        cur.execute("INSERT INTO users (user_id, username) VALUES (?,?)", (user_id, username))
+        con.commit()
+        return True
+
+    elif not user[3]:
+        return True
+
+    return False
+
+
 
 
 
