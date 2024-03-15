dfb08c30de1847b3531133452f022cefdc90f89c
Winchestor2001 <sbexruz2001@gmail.com>
2024-03-15 16:23:08 +0500

    add new (2) tables

--- db/database.py
+++ db/database.py
@@ -10,12 +10,30 @@ async def sql_connector():
 
 async def create_tables():
     con, cur = await sql_connector()
+
     cur.execute('''CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
         username VARCHAR(100) NOT NULL,
         lang VARCHAR(10)
     )''')
+
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS category(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category_name VARCHAR(100) NOT NULL,
+            category_img TEXT NOT NULL
+        )""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS products(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                product_name VARCHAR(100) NOT NULL,
+                mini_product_price REAL NOT NULL,
+                big_product_price REAL NOT NULL,
+                product_img TEXT NOT NULL,
+                category_id INTEGER NOT NULL,
+                FOREIGN KEY (category_id) REFERENCES category(id)
+    )""")
 
 
 
